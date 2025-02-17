60b3bccf8dc9b8f55fba05e6440d8787dae6acfd
David Minaev <95426617+globalarray@users.noreply.github.com>
2025-02-17 23:24:09 +0300

    Create number21_1.py

---
+++ 17_02_2025/number21_1.py
@@ -0,0 +1,15 @@
+def f(x, p):
+    if (p == 4 or p == 2) and x > 39:
+        return 1
+    elif p == 4 and x < 40:
+        return 0
+    elif p < 4 and x > 39:
+        return 0
+    else:
+        if p % 2:
+            return f(x + 1, p + 1) or f((x * 3) - 2, p + 1)
+        else:
+            return f(x + 1, p + 1) and f((x * 3) - 2, p + 1)
+for j in range(1, 40):
+    if f(j, 0) == 1:
+        print("Задание 21:", j)
