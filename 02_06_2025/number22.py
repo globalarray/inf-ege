e834fce90fcc023c4f9acefc744be44a554b2e1b
David Minaev <95426617+globalarray@users.noreply.github.com>
2025-06-02 13:37:40 +0300

    Create number22.py

---
+++ 02_06_2025/number22.py
@@ -0,0 +1,39 @@
+f = open("S3scvYr5Y.csv", 'r').readlines()
+
+procTimes = dict()
+
+def set_running_time(pData):
+    idx = int(pData[0])
+    selfTime = int(pData[1])
+
+    if idx in procTimes:
+        return
+
+    if pData[2] == '0':
+        procTimes[idx] = selfTime
+        return
+
+    dependProcIds = map(int, pData[2].split(';'))
+
+    time = 0
+
+    for dpdId in dependProcIds:
+        if dpdId not in procTimes:
+            set_running_time(f[dpdId].split())
+        time = max(time, procTimes[dpdId])
+
+    procTimes[idx] = time + selfTime
+
+for line in f[1:]:
+    set_running_time(line.split())
+
+for i in range(max(procTimes.values())+1):
+    cnt = 0
+
+    for procId, procEndTime in procTimes.items():
+        procStartTime = procEndTime - int(f[procId].split()[1])
+
+        if procStartTime <= i < procEndTime:
+            cnt += 1
+    if cnt == 4:
+        print(i, cnt)
