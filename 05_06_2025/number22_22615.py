bb75460025179c59f54566ac47eb1b206031ee3f
David Minaev <95426617+globalarray@users.noreply.github.com>
2025-06-05 16:37:59 +0300

    Create number22_22615.py

---
+++ 05_06_2025/number22_22615.py
@@ -0,0 +1,38 @@
+f = open("22_05.txt", 'r').readlines()
+
+procTimes = dict()
+
+def set_running_time(pData):
+    idx = int(pData[0])
+    selfTime = int(pData[1])
+
+    if idx in procTimes:
+        return
+
+    if pData[2] == '0':
+        procTimes[idx] = selfTime
+        return
+
+    dependProcIds = map(int, pData[2].split(';'))
+
+    time = 0
+
+    for dependProcId in dependProcIds:
+        if dependProcId not in procTimes:
+            set_running_time(f[dependProcId].split())
+
+        time = max(time, procTimes[dependProcId])
+    procTimes[idx] = selfTime + time
+
+for line in f[1:]:
+    set_running_time(line.split())
+
+for i in range(max(procTimes.values())+1):
+    cnt = 0
+
+    for procIdx, procEndTime in procTimes.items():
+        procStartTime = procEndTime - int(f[procIdx].split()[1])
+
+        if procStartTime <= i < procEndTime:
+            cnt += 1
+    print(i, cnt)
