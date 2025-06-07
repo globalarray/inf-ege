5f8df9a080138fbca8ccc6fb19b86a40507e2b45
David Minaev <95426617+globalarray@users.noreply.github.com>
2025-06-07 10:55:04 +0300

    Create number22_20192.py

---
+++ 07_06_2025/number22_20192.py
@@ -0,0 +1,32 @@
+f = open("22.7_20192.csv", 'r').readlines()
+
+procTimes = dict()
+
+def set_running_time(pData, t):
+    idx = int(pData[0])
+
+    if pData[1] == 't':
+        selfTime = t
+    else:
+        selfTime = int(pData[1])
+
+    if idx in procTimes:
+        return
+
+    if pData[2] == '0':
+        procTimes[idx] = selfTime
+        return
+
+    dependProcIds = map(int, pData[2].split(';'))
+
+    time = 0
+
+    for dependProcId in dependProcIds:
+        if dependProcId not in procTimes:
+            set_running_time(f[dependProcId - 100].split(), t)
+        time = max(time, procTimes[dependProcId])
+    procTimes[idx] = selfTime + time
+
+for line in f[1:]:
+    set_running_time(line.split(), 10)
+print(max(procTimes.values()))
